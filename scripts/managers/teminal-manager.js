5ef4c094051627f04387f95a4711c8039c733b54
Quantyl <101029269+Quantyl@users.noreply.github.com>
2023-12-13 21:50:07 +0100

    Commit 8 - Command Definition

--- scripts/managers/teminal-manager.js
+++ scripts/managers/teminal-manager.js
@@ -1,41 +1,55 @@
 import Interface from "./interface-manager.js";
 
-const TREE =[
-        [0],
-      [1,1,2],
-    [1,4,1,2,5],
-  [1,4,1,3,2,3,2],
-[5,1,6,1,2,3,2,4,2],
-        [7],
-      [8,7,9]
-];
-const TREE_CHARS = [
-    "+",
-    "<",
-    ">",
-    "$",
-    ",",
-    "@",
-    "&",
-    "|",
-    "/",
-    "\\"
-];
-const TREE_COLORS = [
-    Interface.STYLE_BOLD + Interface.COLOR_YELLOW,
-    Interface.STYLE_RESET + Interface.COLOR_GREEN,
-    Interface.STYLE_RESET + Interface.COLOR_GREEN,
-    Interface.STYLE_RESET + Interface.COLOR_BLUE,
-    Interface.STYLE_RESET + Interface.COLOR_GREEN,
-    Interface.STYLE_RESET + Interface.COLOR_MAGENTA,
-    Interface.STYLE_RESET + Interface.COLOR_RED,
-    Interface.STYLE_RESET + Interface.COLOR_BLACK,
-    Interface.STYLE_RESET + Interface.COLOR_BLACK,
-    Interface.STYLE_RESET + Interface.COLOR_BLACK,
-];
-const LINE_STARTER = "Advent-of-Code>";
+export class Command{
+    static FAILURE_CODE = 0;
+    static SUCCESS_CODE = 1;
+
+    constructor(execMessage, cmndPrefix){
+        this.execMessage = execMessage;
+        this.cmndPrefix = cmndPrefix;
+    }
+
+    execute() { return [Command.FAILURE_CODE, "Execution method not implemented"]; }
+}
 
 export class Terminal{
+    #TREE =[
+        [0],
+        [1,1,2],
+        [1,4,1,2,5],
+        [1,4,1,3,2,3,2],
+        [5,1,6,1,2,3,2,4,2],
+        [7],
+        [8,7,9]
+    ];
+    #TREE_CHARS = [
+        "+",
+        "<",
+        ">",
+        "$",
+        ",",
+        "@",
+        "&",
+        "|",
+        "/",
+        "\\"
+    ];
+    #TREE_COLORS = [
+        Interface.STYLE_BOLD + Interface.COLOR_YELLOW,
+        Interface.STYLE_RESET + Interface.COLOR_GREEN,
+        Interface.STYLE_RESET + Interface.COLOR_GREEN,
+        Interface.STYLE_RESET + Interface.COLOR_BLUE,
+        Interface.STYLE_RESET + Interface.COLOR_GREEN,
+        Interface.STYLE_RESET + Interface.COLOR_MAGENTA,
+        Interface.STYLE_RESET + Interface.COLOR_RED,
+        Interface.STYLE_RESET + Interface.COLOR_BLACK,
+        Interface.STYLE_RESET + Interface.COLOR_BLACK,
+        Interface.STYLE_RESET + Interface.COLOR_BLACK,
+    ];
+
+    #LINE_STARTER = "Advent-of-Code>";
+
+
     constructor(){
         this.#enter();
         this.#update();
@@ -43,10 +57,10 @@ export class Terminal{
 
     #enter(){
         function generateTree(){
-            return TREE.reduce((res, val, index) => {
+            return this.#TREE.reduce((res, val, index) => {
                 let padding = "".padStart(4 - ((val.length - 1) / 2));
                 let row = val.reduce((res, val) => {
-                    return res += TREE_COLORS[val] + TREE_CHARS[val];
+                    return res += this.#TREE_COLORS[val] + this.#TREE_CHARS[val];
                 }, padding);
 
                 switch(index){
@@ -71,7 +85,16 @@ export class Terminal{
 
         while(run){
             Interface.input(LINE_STARTER);
-            // To-do : Add commands.
+            /* To-do : Add commands: 
+                -init-day __index__
+                -read-day __index__ (returns time to run, completed-status, etc. from day__index__.json)
+                -run-day __index__ (runs day, and if no errors, return time taken, etc.)
+                -set-day __index__ (set the info for day__index__.json)
+            */
         }
+    }
+
+    logError(error, message){
+
     }
 }
